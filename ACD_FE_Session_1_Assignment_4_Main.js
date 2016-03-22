06ea377d2af5b910efcb11062bd9f6493024ba2e
ratnak1990 <ratnak1990@gmail.com>
2016-03-22 11:57:41 +0530

    Submitting assignments

---
+++ ACD_FE_Session_1_Assignment_4_Main.js
@@ -0,0 +1,8 @@
+var person={
+        name:"Aamir",
+        age:28,
+        city:"Mumbai"
+};
+document.getElementById("Name").innerHTML =person.name;
+document.getElementById("Age").innerHTML =person.age;
+document.getElementById("City").innerHTML =person.city;
